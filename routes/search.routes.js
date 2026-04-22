d62de6b8ec787fa4b551d33cad9228c233491697
Your Name <you@example.com>
2026-04-22 08:35:16 +0530

    Feat: Watchlist management - add/remove products functionality

--- routes/search.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { searchProducts } from "../controllers/search.controller.js";
-
-const router = Router();
-
-// GET /api/search?q=<keyword>
-router.get("/", searchProducts);
-
-export default router;
